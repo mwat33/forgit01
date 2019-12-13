bfbd122b5de998be41fe97cb6a387a001b84bef5
mwat33 <58013466+mwat33@users.noreply.github.com>
2019-12-14 08:36:23 +0900

    master

--- windy.js
+++ windy.js
@@ -28,34 +28,5 @@ windyInit(options, windyAPI => {
         .openOn(map);
     */
 
-   const { picker, utils, broadcast } = windyAPI;
-
-   picker.on('pickerOpened', latLon => {
-       // picker has been opened at latLon coords
-       console.log(latLon);
-
-       const { lat, lon, values, overlay } = picker.getParams();
-       // -> 48.4, 14.3, [ U,V, ], 'wind'
-       console.log(lat, lon, values, overlay);
-
-       const windObject = utils.wind2obj(values);
-       console.log(windObject);
-   });
-
-   picker.on('pickerMoved', latLon => {
-       // picker was dragged by user to latLon coords
-       console.log(latLon);
-   });
-
-   picker.on('pickerClosed', () => {
-       // picker was closed
-   });
-
-   // Wait since wather is rendered
-   broadcast.once('redrawFinished', () => {
-       picker.open({ lat: 34.718, lon: 137.562 });
-       // Opening of a picker (async)
-   });
-
 
 });
