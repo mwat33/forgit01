73d3d7c3ba6f420e5a7995b792bba692b8d3ebb8
watanabe <watanabe@sis.local>
2019-12-17 11:32:19 +0900

    master

--- windy.js
+++ windy.js
@@ -15,18 +15,10 @@ const options = {
 
 // Initialize Windy API
 windyInit(options, windyAPI => {
-    // windyAPI is ready, and contain 'map', 'store',
-    // 'picker' and other usefull stuff
-
     const { map } = windyAPI;
     // .map is instance of Leaflet map
 
-    /*
-    L.popup()
-        .setLatLng([34.718, 137.562])
-        .setContent('')
-        .openOn(map);
-    */
+    overlays.wind.setMetric('m/s');
 
 
 });
